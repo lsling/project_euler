97acc7c2dd40a5f82e925fabc37b24956aa4016a
Laura Slingerland <laura.sling@gmail.com>
2017-11-13 11:22:39 -0500

    Problem 3 solved

--- project_euler3.js
+++ project_euler3.js
@@ -1,3 +1,14 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
-//
 // What is the largest prime factor of the number 600851475143 ?
+
+function largestPrime(n) {
+  for(var i = 2; i <= n; i++) {
+    if(n % i == 0){
+      n /= i;
+      i--;
+    }
+  }
+  return i;
+}
+
+largestPrime(600851475143);
